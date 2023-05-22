9b1f9e66b9e995162c0968bcaa84d7802b0155a7
dongwook <kdw950108@gmail.com>
2023-05-22 11:25:24 +0900

    study - 자료형 1. 문자열, 숫자, boolean

---
+++ src/chap_01/_02_DataTypes.java
@@ -0,0 +1,20 @@
+package chap_01;
+
+/**
+ * @title : 자료형
+ */
+public class _02_DataTypes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World"); // 문자열
+        System.out.println("안녕하세요");
+        System.out.println(11); //숫자
+        System.out.println(-33);
+        System.out.println(3.14);
+        System.out.println(true); //boolean
+        System.out.println(false);
+
+        System.out.println(123 + 233);
+    }
+
+}
