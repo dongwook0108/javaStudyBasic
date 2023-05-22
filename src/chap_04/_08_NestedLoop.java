d7f657cc66e95511831f00f0efe6bc969f430ae0
dongwook <kdw950108@gmail.com>
2023-05-22 23:59:39 +0900

    study - 이중 반복문 별짓기

--- src/chap_04/_08_NestedLoop.java
+++ src/chap_04/_08_NestedLoop.java
@@ -9,7 +9,7 @@ public class _08_NestedLoop {
     public static void main(String[] args) {
         //이중 반복문
 
-        // 별 (*) 사각형 만들기
+        // 별 (*) 왼쪽 사각형 만들기
 
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
@@ -27,6 +27,28 @@ public class _08_NestedLoop {
             System.out.println();
         }
 
+        System.out.println("-------------------");
+        // 별 (*) 오른쪽 사각형 만들기
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 4 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("-------------------");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 5 - i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
 
     }
