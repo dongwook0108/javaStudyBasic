3e3784bc3d69303c436756ae863b539e47ea4896
dongwook <kdw950108@gmail.com>
2023-05-22 14:27:56 +0900

    study - 비교 연산자

---
+++ src/chap_02/_03_Operator3.java
@@ -0,0 +1,32 @@
+package chap_02;
+
+/**
+ * @Title : 연산자
+ * @Content : 비교 연산자
+ */
+public class _03_Operator3 {
+
+    public static void main(String[] args) {
+        System.out.println(5 > 3); // 5는 3보다 큼 -> 참
+        System.out.println(5 >= 3); // 5는 3보다 크거나 같다 -> 참
+        System.out.println(5 >= 5); // 5는 5보다 크거나 같다 -> 참
+        System.out.println(5 >= 7); // 5는 7보다 크거나 같다 -> 거짓
+
+        System.out.println(5 < 3); // 5는 3보다 작다 false
+        System.out.println(5 <= 3); // 5는 3보다 작거나 같다 false
+        System.out.println(3 <= 3); // 5는 3보다 작거나 같다 true
+
+        System.out.println(5 == 5); // 값 비교 T
+        System.out.println(5 == 4); // 값 비교 F
+        System.out.println(5 != 4); // 값 비교 T
+
+
+
+
+
+
+
+
+    }
+
+}
