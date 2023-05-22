81d9a8c39831f662d643b128847a96c26c4d8788
dongwook <kdw950108@gmail.com>
2023-05-22 13:53:52 +0900

    study - quiz -버스 도착 정보를 출력하는 프로그램

---
+++ src/chap_01/_Quiz_01.java
@@ -0,0 +1,21 @@
+package chap_01;
+
+/**
+ * 문제 - 버스 도착 정보를 출력하는 프로그램
+ */
+public class _Quiz_01 {
+
+    public static void main(String[] args) {
+
+        String busNumber = "상암08번";
+        int minute = 3;
+        float distance = 1.2F;
+
+        System.out.println(busNumber + "버스");
+        System.out.println("약 " + minute + "후 도착");
+        System.out.println("남은 거리 " + distance + "km");
+
+    }
+
+
+}
