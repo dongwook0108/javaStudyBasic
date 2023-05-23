82da4f7418c960e4fa97a21a75ff2dc756fb7418
dongwook <hal0110@naver.com>
2023-05-24 00:11:46 +0900

    study - quiz 배열을 활용하여 쇼핑몰에서 구매 가능한 신발 사이즈 옵션을 출력하는 프로그램

---
+++ src/chap_05/_Quiz_05.java
@@ -0,0 +1,24 @@
+package chap_05;
+
+/**
+ * 배열을 활용하여 쇼핑몰에서 구매 가능한 신발 사이즈 옵션을 출력하는 프로그램
+ * 조건
+ * 1. 신발 사이즈는 250부터 295까지 5단위로 증가
+ * 2. 사이즈 수는 10가지
+ *
+ */
+public class _Quiz_05 {
+
+    public static void main(String[] args) {
+
+        int[] sizeArray = new int[10];
+        for (int i = 0; i < sizeArray.length; i++) {
+            sizeArray[i] = 250 + (5 * i);
+        }
+
+        for (int size : sizeArray) {
+            System.out.println("사이즈 " + size + " 재고 있음");
+        }
+
+    }
+}
