ba4ea73097ab5a767d67209aaf2204bbc0a84746
dongwook <hal0110@naver.com>
2023-05-24 23:59:38 +0900

    study - 생성자 1. 객체가 만들어질 때 자동으로 호출되는 메소드

--- src/chap_07/BlackBox.java
+++ src/chap_07/BlackBox.java
@@ -7,8 +7,29 @@ public class BlackBox {
     int price;
     String color;
 
+    int serialNumber;
+
+    static int counter = 0;
+
     static boolean canAutoReport = false; // 자동 신고 기능
     // static 선언 시 클래스 변수
+
+    //생성자 만드는 법
+    public BlackBox() {
+        System.out.println("기본 생성자 호출");
+        this.serialNumber = ++counter;
+        System.out.println("새로운 시리얼 넘버를 발급 받았습니다. " + this.serialNumber);
+    }
+
+    public BlackBox(String modelName, String resolution, int price, String color) {
+        this(); // 기본 생성자 호출
+        System.out.println("사용자 정의 생성자 호출");
+        this.modelName = modelName;
+        this.resolution = resolution;
+        this.price = price;
+        this.color = color;
+    }
+
 
 
     public void autoReport() {
