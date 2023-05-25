9bbad016d9a4fd10045265355853cfae16c4af1e
dongwook <hal0110@naver.com>
2023-05-25 22:21:19 +0900

    study -  제네릭스 1. 제네릭스에서 지원하는거는 -> 객체 2. 기본형들은 변환해줘야함

---
+++ src/chap_09/_01_Generics.java
@@ -0,0 +1,57 @@
+package chap_09;
+
+/**
+ * @Title : 제네릭스
+ * @Content : 제네릭스에서 지원하는거는 -> 객체
+ * 기본형들은 변환해줘야함
+ */
+public class _01_Generics {
+
+    public static void main(String[] args) {
+        Integer[] iArray = {1, 2, 3, 4, 5};
+        Double[] dArray = {1.0, 2.0, 3.0, 4.0, 5.0};
+        String[] sArray = {"A", "B", "C", "D", "E"};
+
+        printIntArray(iArray);
+        printDoubleArray(dArray);
+        printStringArray(sArray);
+
+        System.out.println("----------------");
+
+        printAnyArray(iArray);
+        printAnyArray(dArray);
+        printAnyArray(sArray);
+    }
+
+    // T = type, K = key, V = value, E = element
+    private static <T> void printAnyArray(T[] array) {
+        for (T t : array) {
+            System.out.print(t + " ");
+        }
+        System.out.println();
+    }
+
+    private static void printStringArray(String[] sArray) {
+        for (String s : sArray) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
+    }
+
+    private static void printDoubleArray(Double[] dArray) {
+        for (double d : dArray) {
+            System.out.print(d + " ");
+        }
+        System.out.println();
+
+    }
+
+    private static void printIntArray(Integer[] iArray) {
+        for (int i : iArray) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+    }
+
+}
