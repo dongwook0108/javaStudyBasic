2a6593d7b4217aedca9a147c974391ce8fe6b1af
dongwook <hal0110@naver.com>
2023-05-26 17:33:50 +0900

    study -  제네릭 클래스

--- src/chap_09/_02_GenericsClass.java
+++ src/chap_09/_02_GenericsClass.java
@@ -4,6 +4,9 @@ import chap_09.coffee.Coffee;
 import chap_09.coffee.CoffeeByName;
 import chap_09.coffee.CoffeeByNickname;
 import chap_09.coffee.CoffeeByNumber;
+import chap_09.coffee.CoffeeByUser;
+import chap_09.user.User;
+import chap_09.user.VIPUser;
 
 /**
  * @Title : 제네릭 클래스
@@ -46,6 +49,29 @@ public class _02_GenericsClass {
         c6.ready();
         String c6Name = c6.name;
         System.out.println(c6Name);
+
+        System.out.println("================");
+
+        CoffeeByUser<User> c7 = new CoffeeByUser<>(new User("김동욱"));
+        c7.ready();
+
+        CoffeeByUser<User> c8 = new CoffeeByUser<>(new VIPUser("김동욱2"));
+        c8.ready();
+
+        System.out.println("================");
+        orderCoffee(36);
+
+        System.out.println("================");
+        orderCoffee("김동욱12", "아아");
+        orderCoffee("37", "라떼");
+    }
+
+    public static <T> void orderCoffee(T name) {
+        System.out.println("커피 주문 완료 : " + name);
+    }
+
+    public static <T, V> void orderCoffee(T name, V coffee) {
+        System.out.println(coffee + "주문 완료 : " + name);
     }
 
 }
