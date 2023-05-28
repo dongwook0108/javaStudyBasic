0559c0b7210af82fc6e62a027e97bbc6c84a6cb2
dongwook <hal0110@naver.com>
2023-05-28 15:50:25 +0900

    study - Runnable - Runnable vs Thread - 다중상속 가능 vs 불가능

---
+++ src/chap_12/clean/CleanRunnable.java
@@ -0,0 +1,13 @@
+package chap_12.clean;
+
+public class CleanRunnable implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("-- 직원 청소 시작 --");
+        for (int i = 2; i <= 10; i += 2) {
+            System.out.println("직원 " + i + "번 방 청소중");
+        }
+        System.out.println("직원 청소끝");
+    }
+}
