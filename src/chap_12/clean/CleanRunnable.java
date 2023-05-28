3408bd4a2e8e4576be124a4ebc1d79014f96db4c
dongwook <hal0110@naver.com>
2023-05-28 16:17:10 +0900

    study - Join - sleep, join 시간을 넣어주면 대기 했다가 시작

--- src/chap_12/clean/CleanRunnable.java
+++ src/chap_12/clean/CleanRunnable.java
@@ -7,6 +7,11 @@ public class CleanRunnable implements Runnable{
         System.out.println("-- 직원 청소 시작 --");
         for (int i = 2; i <= 10; i += 2) {
             System.out.println("직원 " + i + "번 방 청소중");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
         System.out.println("직원 청소끝");
     }
